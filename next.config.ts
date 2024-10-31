f33e58357eed901b10ec718d516a29ca84d64f92
Ivan Strilets <i.strilets@vk.team>
2024-10-31 23:27:47 +0300

    update config file

--- next.config.ts
+++ next.config.ts
@@ -6,29 +6,45 @@ const nextConfig: NextConfig = {
 
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const fileLoaderRule = config.module.rules.find((rule: any) =>
-      rule.test?.test?.(".svg")
-    );
+    // const fileLoaderRule = config.module.rules.find((rule) =>
+    //   rule.test?.test?.(".svg")
+    // );
 
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
+    // config.module.rules.push(
+    //   // Reapply the existing rule, but only for svg imports ending in ?url
+    //   {
+    //     ...fileLoaderRule,
+    //     test: /\.svg$/i,
+    //     resourceQuery: /url/, // *.svg?url
+    //   },
+    //   // Convert all other *.svg imports to React components
+    //   {
+    //     test: /\.svg$/i,
+    //     issuer: fileLoaderRule.issuer,
+    //     resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+    //     use: ["@svgr/webpack"],
+    //   }
+    // );
+
+    // // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    // fileLoaderRule.exclude = /\.svg$/i;
+
+    // return config;
+
+    config.module?.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        and: [/\.(js|ts|jsx|tsx)$/],
       },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            // здесь вы можете указать настройки для svgr, если необходимо
+          },
+        },
+      ],
+    });
 
     return config;
   },
