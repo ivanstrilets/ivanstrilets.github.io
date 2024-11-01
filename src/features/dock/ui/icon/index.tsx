e2f0fe5e10dcf17057de0cecc40a278d2890f20a
Ivan Strilets <i.strilets@vk.team>
2024-11-01 14:41:37 +0300

    feature/dock: update

---
+++ src/features/dock/ui/icon/index.tsx
@@ -0,0 +1,15 @@
+import { paths } from "./svg";
+
+type Icon = keyof typeof paths;
+
+interface DockIconProps {
+  icon: Icon;
+}
+
+export const DockIcon = ({ icon }: DockIconProps) => {
+  return (
+    <svg viewBox="0 0 24 24" role="img">
+      <path d={paths[icon]} />
+    </svg>
+  );
+};
